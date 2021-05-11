e78a019aca10a2773140f8d035f7782b339d523c
Alval2001 <75988110+Alval2001@users.noreply.github.com>
2021-05-11 23:40:58 +0300

    gotovo

---
+++ Exam/app/config.py
@@ -0,0 +1,4 @@
+MYSQL_USER = 'std_1082_exam_web'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
+MYSQL_DATABASE = 'std_1082_exam_web'
+MYSQL_PASSWORD = 'Alval1561'
---
+++ lab_4/app/config.py
@@ -0,0 +1,5 @@
+SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
+MYSQL_USER = 'std_1082'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
+MYSQL_DATABASE = 'std_1082'
+MYSQL_PASSWORD = 'Alval1561'
